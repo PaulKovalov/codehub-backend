5160318cf84d4d1abca7474b872cb73df5bcd6ea
paul <soapmactavish799@gmail.com>
2020-04-17 14:18:25 +0300

    Fix nginx.conf, remove save-images.sh

--- save-images.sh
+++
@@ -1,8 +0,0 @@
-echo "Saving codehub-backend ... "
-docker save -o codehub-backend.tar codehub-backend
-echo "Saving codehub-nginx ... "
-docker save -o codehub-nginx.tar codehub-nginx
-echo "Saving codehub-postgres ... "
-docker save -o codehub-postgres.tar codehub-postgres
-echo "Saving codehub-frontend ... "
-docker save -o codehub-frontend.tar codehub-frontend
