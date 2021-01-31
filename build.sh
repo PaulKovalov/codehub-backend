36bc13d9ca69f95ecada2d9ad28510469ea2fc6e
paul <soapmactavish799@gmail.com>
2021-01-31 20:56:07 +0200

    Migrate from postgres-in-docker to postgres-on-host

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@ echo "Started building backend ..."
 docker build -t codehub-backend:latest -f dockerfiles/django .
 echo "Started building nginx image ..."
 docker build -t codehub-nginx:latest -f dockerfiles/nginx .
-echo "Started building postgres image ..."
-docker build -t codehub-postgres:latest -f dockerfiles/postgres .
 echo "Started building celery image ..."
 docker build -t codehub-celery:latest -f dockerfiles/celery .
 echo "Started building celery-beat image ..."
